002e0c1f2f6b7f175a494b43e3528b8114c035fb
Luisa Porraso <luisaf.porrasg@ecci.edu.co>
2024-03-06 21:35:47 -0500

    Versión 1.0 web + enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PerfilComponent } from './perfil/perfil.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'estudios', component: EducacionComponent },
+  { path: 'experiencia', component: ExperienciaComponent },
+  { path: 'experiencia', component: PortafolioComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '', redirectTo: '/perfil', pathMatch: 'full' }, // Ruta por defecto
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
